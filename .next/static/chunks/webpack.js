86f57491cf08e49d9d2722177b3b2936ccdb3926
Arnie Hayes <arniehayes1@gmail.com>
2022-04-10 19:31:43 -0700

    nav safari bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "38c53cd68101f398"; }
+/******/ 		__webpack_require__.h = function() { return "48b78b51a3195a92"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
