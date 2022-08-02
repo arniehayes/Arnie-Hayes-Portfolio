9d507984bf3b5b133aae7e317d9ac0f06b50303e
Arnie Hayes <47344710+arniehayes@users.noreply.github.com>
2022-08-01 20:33:32 -0700

    Update Resume.js

--- components/MainPage/Body/Hero/Resume.js
+++ components/MainPage/Body/Hero/Resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
       <section className={style.resume_section}>
         <div className={style.resume_button_container} id="resume">
           <motion.a
-            href="https://drive.google.com/uc?export=download&id=1I3aIIfgPDeDQJ0tKewHa9HixAaDp09Gg"
+            href="https://drive.google.com/file/d/1NcQZt0h57GiJAVl49HSIeZ0uNQzK0bL4"
             aria-label="Resume Download"
             target="_blank"
             rel="noopener noreferrer"
